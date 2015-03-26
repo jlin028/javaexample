2e3992cd4a26523cddcf18e97e50b5fcad6f2128
jlin028 <jlin028@gmail.com>
2015-03-25 22:02:19 -0500

    <priv> another test

--- examples/src/TestDrive.java
+++ examples/src/TestDrive.java
@@ -5,6 +5,27 @@ public class TestDrive {
 	{
 		System.out.println("Hello Github!");
 		System.out.println("second test!");
+		
+		System.out.println(isPalindrome("Abc1221cba"));
+		System.out.println(isPalindrome("a123bc"));
+	}
+	
+	public static boolean isPalindrome(String s)
+	{
+		int i = 0;
+		int j = s.length() -1;
+		while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+		while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+		while (i < j)
+		{
+			if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+			{
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
 	}
 
 }
